b089fc25bc12728160045179df87379117e6a149
SMaelyss <smndjomon@protonmail.com>
2022-07-04 15:26:09 +0100

    adding data files and the file to create the tables for db

---
+++ datafiles/elements_manipulator.py
@@ -0,0 +1,60 @@
+"""
+File: elements_manipulator.py
+
+Version: V1.2
+Date: 04.07.2022
+Function: Obtains the list of elements and element types and transform them into sql values format for table insertion.
+
+Author: Sarah Maelyss N'djomon
+------------------------------------------------------------------------------------------------------------------------
+Description
+===========
+This table takes a list of all the cds, srna, utr and ncrna element's elemt_ids and element_types and places them into
+the values format needed for insertion into the sql element table. This involves surrounding each item by quotations,
+separating them with a comma and encompassing them in brackets. Example: ('element_id', 'element_type').
+
+------------------------------------------------------------------------------------------------------------------------
+calls all_elements.txt
+is called by tables_sql.py
+
+"""
+
+
+# import and open the file with the element ids and types
+all_elements_raw = 'all_elements.txt'
+
+all_elements_list = list()
+with open(all_elements_raw, 'r') as f:
+    all_elements_list = f.read().split()
+
+
+# Extract from the opened file, the element ids and element types
+counter = 0
+element_id_list = []
+element_type_list = []
+for i in all_elements_list:
+    counter += 1
+    if counter % 2 != 0:
+        element_id_list.append(i)
+    else:
+        element_type_list.append(i)
+
+# Create dictionary for the element id and element types
+em_id = element_id_list # key
+em_type = element_type_list # value
+
+zipper = zip(em_id, em_type)
+em_dict = dict(zipper)
+
+# create a variable with the element ids and types in the sql values format
+em_fin = str()
+for k, v in em_dict.items():
+    em_fin = f'{em_fin}(\'{k}\', \'{v}\'),\n'
+
+
+
+
+
+
+
+
---
+++ datafiles/ncrna_insertinto.py
@@ -0,0 +1,56 @@
+
+"""
+File: ncrna_insertinto.py
+
+Version: V1.1
+Date: 03.07.2022
+Function: ncrna table items from research data excel file, formatted for sql in MrDataConverter:
+ref: https://shancarter.github.io/mr-data-converter/.
+
+Author: Sarah Maelyss N'djomon
+------------------------------------------------------------------------------------------------------------------------
+Description
+===========
+This file provides a variable containing the insert into function for populating the ncrna table.
+
+------------------------------------------------------------------------------------------------------------------------
+
+is called by tables_sql.py
+
+"""
+
+
+values = """ INSERT INTO ncrna
+  (ncrna_element_id,ncrna_name,related_srna_name) 
+VALUES 
+  ('EBG00000313378','F6','ncRv10243A'),
+  ('EBG00000313327','mcr19','ncRv0485'),
+  ('EBG00000313383','B55','ncRv10609A'),
+  ('EBG00000313364','ASdes','ncRv0824'),
+  ('EBG00000313344','mpr5','ncRv11051'),
+  ('EBG00000313338','MTS0858','ncRv1092c'),
+  ('EBG00000313317','mcr10','ncRv1157'),
+  ('EBG00000313387','ncrMT1234','ncRv11196'),
+  ('EBG00000313390','mpr6','ncRv1222'),
+  ('EBG00000313343','mcr11','ncRv11264c'),
+  ('EBG00000313377','mcr3','ncRv11315A'),
+  ('EBG00000313340','mcr15','ncRv1364c'),
+  ('EBG00000313353','MTS1082','ncRv11373'),
+  ('EBG00000313337','G2','ncRv11689c'),
+  ('EBG00000313388','AS1726','ncRv1726c'),
+  ('EBG00000313376','MTS1338','ncRv11733A'),
+  ('EBG00000313322','AS1890','ncRv1890'),
+  ('EBG00000313323','ASpks','ncRv2048'),
+  ('EBG00000313321','mcr5','ncRv2175c'),
+  ('EBG00000313389','mcr16','ncRv2243c'),
+  ('EBG00000313380','mcr7','ncRv0024'),
+  ('EBG00000313368','mpr11','ncRv12560'),
+  ('EBG00000313313','mpr12','ncRv12562'),
+  ('EBG00000313367','mpr17','ncRv13596'),
+  ('EBG00000313385','mpr18','ncRv13651'),
+  ('EBG00000313324','B11','ncRv13660c'),
+  ('EBG00000313318','MTS2823','ncRv13661'),
+  ('EBG00000313354','C8','ncRv13722Ac'),
+  ('EBG00000313356','ncrMT3949','ncRv3842'),
+  ('EBG00000313375','MTS2975','ncRv13943');
+"""
